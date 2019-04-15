c93f81fe863d8ebbfc410a4badb72d1b184111fd
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-15 15:22:12 +0200

    connected to postgresql

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import models, { sequelize } from "../models";
+
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!!!!");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server on port ${process.env.PORT}!`)
-);
+sequelize.sync().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}!`);
+  });
+});
