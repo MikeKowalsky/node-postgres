adc12b32ed4e91a876eae0128efb059e1e6e9e3c
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-15 17:52:52 +0200

    added routes, working example

--- src/index.js
+++ src/index.js
@@ -2,19 +2,72 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import models, { sequelize } from "../models";
+import routes from "../routes";
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin("mkowalewski")
+  };
+  next();
 });
 
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
+
+// const eraseDatabaseOnSync = true;
+
 sequelize.sync().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}!`);
-  });
+  // sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
+  // if (eraseDatabaseOnSync) {
+  //   createUsersWithMessages();
+  // }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Server on port ${process.env.PORT}!`)
+  );
 });
+
+// const createUsersWithMessages = async () => {
+//   await models.User.create(
+//     {
+//       username: "mkowalewski",
+//       messages: [
+//         {
+//           text: "Playing with posgresql."
+//         }
+//       ]
+//     },
+//     {
+//       include: [models.Message]
+//     }
+//   );
+
+//   await models.User.create(
+//     {
+//       username: "kmarshall",
+//       messages: [
+//         {
+//           text: "Happy to see it!"
+//         },
+//         {
+//           text: "Show me something!"
+//         }
+//       ]
+//     },
+//     {
+//       include: [models.Message]
+//     }
+//   );
+// };
